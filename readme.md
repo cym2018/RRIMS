86d99e01b073d6a8e82940e2cc0e405322525213
cym2018 <cym2018.xyz@qq.com>
2019-12-14 13:37:08 +0800

    1

---
+++ readme.md
@@ -0,0 +1,15 @@
+# RRIMS
+餐馆订餐信息管理系统
+
+##2019年12月14日 13点27分
+准备工作
+
+1.测试整合spring的applicationContext.xml配置文件
+
+2.测试log4j日志功能
+
+3.测试springJDBC
+
+4.连接数据库
+
+5.servlet
---
+++ src/main/java/test/SpringJDBCTest.java
@@ -0,0 +1,44 @@
+package test;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+public class SpringJDBCTest {
+    static Logger logger = LogManager.getLogger(SpringJDBCTest.class);
+    static ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+    public static void main(String[] args) {
+//        xml();
+//        code();
+        xml2();
+    }
+
+    // 代码实现数据库访问
+    public static void code() {
+        DriverManagerDataSource dmds = new DriverManagerDataSource();
+        dmds.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dmds.setUrl("jdbc:mysql://localhost:3306/RRIMS?serverTimezone=CST");
+        dmds.setUsername("root");
+        dmds.setPassword("ab370126");
+
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dmds);
+        jdbcTemplate.update("insert into test values (1)");
+    }
+
+    // xml+代码实现数据库访问
+    public static void xml() {
+        DriverManagerDataSource dmds = (DriverManagerDataSource) ac.getBean("dataSource");
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dmds);
+        jdbcTemplate.update("insert into test values (2)");
+    }
+
+    // 纯xml实现数据库访问
+    static void xml2() {
+        JdbcTemplate jt = (JdbcTemplate) ac.getBean("jdbcTemplate");
+        jt.update("insert into test values (3)");
+    }
+}
